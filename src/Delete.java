ca935f358adde1387117c82b3e49a7c4374a2887
ser-jpg <userkansen@gmail.com>
2019-11-30 20:59:08 -0500

    added a new methods about max number

--- src/Delete.java
+++ src/Delete.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Delete {
 
     public static void main(String[] args) {
@@ -13,6 +15,22 @@ public class Delete {
         System.out.println("sacma bir odev");
         System.out.println("sacma bir odev");
         System.out.println("sacma bir odev");
+    }
+    public void findMaxNumber(){
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("please eneter two number");
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        if(num1>num2){
+
+            System.out.println("num1 is"+num1+"max");
+        }
+        else{
+            System.out.println("num2 is"+num2+"max");
+        }
+
+
     }
 
 }
